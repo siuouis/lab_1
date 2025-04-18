860ca8f6416c9497fc32077cc88e78ea172a8030
Nick <weter905@gmail.com>
2025-04-18 21:35:05 +0500

    Realisation Function_1.cpp

--- Function_1.cpp
+++ Function_1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "function1.h"
 
+using namespace std;
+
+void readDataFromFile(const string& fileName, vector<string>& lines) {
+    ifstream inputFile(fileName);
+    if (inputFile.is_open()) {
+        string line;
+        while (getline(inputFile, line)) {
+            lines.push_back(line);
+        }
+        inputFile.close();
+    }
+}
